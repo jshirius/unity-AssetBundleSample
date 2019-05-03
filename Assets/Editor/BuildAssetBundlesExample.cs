a646a6b34bcc88ea8b198b1e50d9e6bb83ed9c5d
taki mac <takizawa@liberteenz.jp>
2019-05-03 15:32:03 +0900

    init files

---
+++ Assets/Editor/BuildAssetBundlesExample.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildAssetBundlesExample : MonoBehaviour {
+
+	//依存関係
+	//PrefabA・・・image1
+	//PrefabB・・・image2
+	//PrefabAB・・・image1とimage2
+
+	[MenuItem("Assets/Build AssetBundles")]
+	static void BuildABs()
+	{
+		//エディタで選択した範囲のものについてAssetbundleを作成する
+		//うまく動作しない？
+		BuildPipeline.BuildAssetBundles("Assets/ABs", BuildAssetBundleOptions.None, BuildTarget.iOS);
+	}
+		
+
+
+	[MenuItem("Example/Build  prefab_bundle_prefabA_only")]
+	static void PrefabBundlePrefabAOnly()
+	{
+		// Create the array of bundle build details.
+		AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+
+
+		buildMap[0].assetBundleName = "prefab_bundle_prefab_A_only";
+		string[] prefabAssets = new string[1];
+		prefabAssets[0] = "Assets/AssetBundle/PrefabAssets/PrefabA.prefab";
+		buildMap[0].assetNames = prefabAssets;
+
+		BuildPipeline.BuildAssetBundles("Assets/ABs", buildMap, BuildAssetBundleOptions.None, BuildTarget.iOS);	
+	}
+		
+
+	[MenuItem("Example/Build prefab_bundle_prefabAB_only")]
+	static void PrefabBundlePrefabABOnly()
+	{
+		// Create the array of bundle build details.
+		AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+
+
+		buildMap[0].assetBundleName = "prefab_bundle_prefab_AB_only";
+		string[] prefabAssets = new string[1];
+		prefabAssets[0] = "Assets/AssetBundle/PrefabAssets/PrefabAB.prefab";
+		buildMap[0].assetNames = prefabAssets;
+
+		BuildPipeline.BuildAssetBundles("Assets/ABs", buildMap, BuildAssetBundleOptions.None, BuildTarget.iOS);	
+	}
+
+	//プレファブとテクスチャを分けたとき、アセットバンドルも分けて作成されるか確認する
+	[MenuItem("Example/Build Asset Bundles Using BuildMap")]
+	static void BuildMapImageAndPrefabABs()
+	{
+		// Create the array of bundle build details.
+		AssetBundleBuild[] buildMap = new AssetBundleBuild[2];
+
+		buildMap[0].assetBundleName = "image_bundle";
+
+		string[]  imageAssets = new string[2];
+		imageAssets[0] = "Assets/AssetBundle/ImageAssets/image1.png";
+		imageAssets[1] = "Assets/AssetBundle/ImageAssets/image2.png";
+
+		buildMap[0].assetNames = imageAssets;
+
+
+		//-------------------------------
+		buildMap[1].assetBundleName = "prefab_bundle";
+		string[] prefabAssets = new string[4];
+		prefabAssets[0] = "Assets/AssetBundle/PrefabAssets/PrefabA.prefab";
+		prefabAssets[1] = "Assets/AssetBundle/PrefabAssets/PrefabB.prefab";
+		prefabAssets[2] = "Assets/AssetBundle/PrefabAssets/PrefabAB.prefab";
+		prefabAssets[3] = "Assets/AssetBundle/PrefabAssets/abc.txt";
+
+		buildMap[1].assetNames = prefabAssets;
+
+		BuildPipeline.BuildAssetBundles("Assets/ABs", buildMap, BuildAssetBundleOptions.None, BuildTarget.iOS);
+	}
+
+
+
+
+	//プレファブの依存関係を見てテクスチャを含めるか確認する
+	[MenuItem("Example/Build Asset Bundles PrefabOnly")]
+	static void BuildMapPrefabOnlyABs()
+	{
+		// Create the array of bundle build details.
+		AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+
+
+		//-------------------------------
+		buildMap[0].assetBundleName = "prefab_bundle";
+		string[] prefabAssets = new string[3];
+		prefabAssets[0] = "Assets/AssetBundle/PrefabAssets/PrefabA.prefab";
+		prefabAssets[1] = "Assets/AssetBundle/PrefabAssets/PrefabB.prefab";
+		prefabAssets[2] = "Assets/AssetBundle/PrefabAssets/PrefabAB.prefab";
+		prefabAssets[3] = "Assets/AssetBundle/PrefabAssets/abc.txt";
+
+		buildMap[0].assetNames = prefabAssets;
+
+		BuildPipeline.BuildAssetBundles("Assets/ABs", buildMap, BuildAssetBundleOptions.None, BuildTarget.iOS);
+	}
+
+
+
+}
---
+++ Assets/Script/SampleLoadAsset.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleLoadAsset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	AssetBundle prefabAsset = null;
+	AssetBundle imageAsset = null;
+
+	public GameObject Canvas;
+	
+	// Update is called once per frame
+	void OnGUI() {
+
+		//アセットバンドルの読み込み
+		if (GUI.Button (new Rect (10, 70, 200, 100), "LoadAsset")) {
+			Debug.Log("Clicked LoadAsset");
+			StartCoroutine (LoadCacheOrDownload ());
+
+		}
+
+		//画像のアセットを削除
+		if (GUI.Button (new Rect (10, 200, 200, 100), "LoadImageAsset")) {
+			Caching.CleanCache();
+			Debug.Log("CleanCache");
+		}
+
+		//キャッシュ削除
+		if (GUI.Button (new Rect (10, 300, 200, 100), "CleanCache")) {
+			Caching.CleanCache();
+			Debug.Log("CleanCache");
+		}
+
+
+		if (GUI.Button (new Rect (10, 400, 200, 100), "PrefabAssetUnload")) {
+			PrefabAssetUnload ();
+			Debug.Log("PrefabAssetUnload");
+		}
+
+
+		if (GUI.Button (new Rect (10, 500, 200, 100), "CreatGameObject")) {
+			CreatGameObject();
+			Debug.Log("CreatGameObject");
+		}
+
+		if (GUI.Button (new Rect (10, 600, 200, 100), "LoadPrefabBundlePrefabAOnly")) {
+			StartCoroutine (LoadPrefabBundlePrefabAOnly ());
+			Debug.Log("LoadPrefabBundlePrefabAOnly");
+		}
+
+		if (GUI.Button (new Rect (10, 700, 200, 100), "LoadPrefabBundlePrefabABOnly")) {
+			StartCoroutine (LoadPrefabBundlePrefabABOnly ());
+			Debug.Log("LoadPrefabBundlePrefabABOnly");
+		}
+
+	}
+	IEnumerator LoadImageAsset(){
+		using (var www = WWW.LoadFromCacheOrDownload("http://localhost/assetbundles/image_bundle", 5))
+		{
+			yield return www;
+			//Debug.Log(String.format "LoadFromCacheOrDownload {0}" , www.assetBundle.ToString());
+
+
+			imageAsset = www.assetBundle;
+			//var asset = myLoadedAssetBundle.mainAsset;
+
+		}
+
+
+	}
+
+	void PrefabAssetUnload(){
+		prefabAsset.Unload (true);
+		imageAsset.Unload (true);
+	}
+
+	void CreatGameObject(){
+		AssetBundleRequest image = prefabAsset.LoadAssetAsync<GameObject>("PrefabA");
+		GameObject obj = (GameObject)Instantiate( image.asset );
+		obj.transform.SetParent(Canvas.gameObject.transform, false);
+
+		image = prefabAsset.LoadAssetAsync<GameObject>("PrefabAB");
+		obj = (GameObject)Instantiate( image.asset );
+		obj.transform.SetParent(Canvas.gameObject.transform, false);
+
+	}
+
+
+	IEnumerator LoadPrefabBundlePrefabAOnly(){
+		Debug.Log("LoadPrefabBundlePrefabAOnly begin");
+
+
+		using (var www = WWW.LoadFromCacheOrDownload("http://localhost/assetbundles/prefab_bundle_prefab_a_only", 5))
+		{
+			yield return www;
+			if (!string.IsNullOrEmpty(www.error))
+			{
+				Debug.Log(www.error);
+				yield return null;
+			}
+
+			//Debug.Log(String.format "LoadFromCacheOrDownload {0}" , www.assetBundle.ToString());
+
+
+			var myLoadedAssetBundle = www.assetBundle;
+
+			Debug.Log("LoadFromCacheOrDownload OK");
+
+			foreach (string s in myLoadedAssetBundle.GetAllAssetNames()) {
+				Debug.Log(s);
+			}
+
+
+			//アセットバンドルデータ
+			//prefabAsset = myLoadedAssetBundle;
+
+			AssetBundleRequest assetdata = myLoadedAssetBundle.LoadAssetAsync<GameObject>("PrefabA");
+			GameObject obj = (GameObject)Instantiate( assetdata.asset );
+			obj.transform.SetParent(Canvas.gameObject.transform, false);
+
+
+		}
+	}
+
+
+
+	IEnumerator LoadPrefabBundlePrefabABOnly(){
+		Debug.Log("LoadPrefabBundlePrefabAOnly begin");
+
+
+		using (var www = WWW.LoadFromCacheOrDownload("http://localhost/assetbundles/prefab_bundle_prefab_ab_only", 5))
+		{
+			yield return www;
+			if (!string.IsNullOrEmpty(www.error))
+			{
+				Debug.Log(www.error);
+				yield return null;
+			}
+
+			//Debug.Log(String.format "LoadFromCacheOrDownload {0}" , www.assetBundle.ToString());
+
+
+			var myLoadedAssetBundle = www.assetBundle;
+
+			Debug.Log("LoadFromCacheOrDownload OK");
+
+			foreach (string s in myLoadedAssetBundle.GetAllAssetNames()) {
+				Debug.Log(s);
+			}
+
+
+			//アセットバンドルデータ
+			//prefabAsset = myLoadedAssetBundle;
+
+			AssetBundleRequest assetdata = myLoadedAssetBundle.LoadAssetAsync<GameObject>("PrefabAB");
+			GameObject obj = (GameObject)Instantiate( assetdata.asset );
+			obj.transform.SetParent(Canvas.gameObject.transform, false);
+
+
+		}
+	}
+
+
+	IEnumerator LoadCacheOrDownload(){
+		Debug.Log("LoadFromCacheOrDownload begin");
+
+		//さきにimageを読み込んでおく必要がある
+		using (var www = WWW.LoadFromCacheOrDownload("http://localhost/assetbundles/image_bundle", 5))
+		{
+			yield return www;
+			//Debug.Log(String.format "LoadFromCacheOrDownload {0}" , www.assetBundle.ToString());
+
+
+			imageAsset = www.assetBundle;
+			//var asset = myLoadedAssetBundle.mainAsset;
+
+		}
+
+		using (var www = WWW.LoadFromCacheOrDownload("http://localhost/assetbundles/prefab_bundle", 5))
+		{
+
+			Debug.Log("LoadFromCacheOrDownload begin2");
+			yield return www;
+			Debug.Log("LoadFromCacheOrDownload begin3");
+			if (!string.IsNullOrEmpty(www.error))
+			{
+				Debug.Log(www.error);
+				yield return null;
+			}
+
+			//Debug.Log(String.format "LoadFromCacheOrDownload {0}" , www.assetBundle.ToString());
+
+
+			var myLoadedAssetBundle = www.assetBundle;
+
+			Debug.Log("LoadFromCacheOrDownload OK");
+
+			foreach (string s in myLoadedAssetBundle.GetAllAssetNames()) {
+				Debug.Log(s);
+			}
+
+
+			//アセットバンドルデータ
+			prefabAsset = myLoadedAssetBundle;
+
+
+
+
+		}
+	}
+}
